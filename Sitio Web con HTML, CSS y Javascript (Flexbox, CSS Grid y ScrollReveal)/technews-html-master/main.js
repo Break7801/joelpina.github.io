b9bca8aeb0822eb410bd90d0336c5a3ef428b5f6
Joel Piña Felipe <Break7801@Gmail.com>
2024-09-30 23:20:30 -0400

    Update main.js

--- Sitio Web con HTML, CSS y Javascript (Flexbox, CSS Grid y ScrollReveal)/technews-html-master/main.js
+++ Sitio Web con HTML, CSS y Javascript (Flexbox, CSS Grid y ScrollReveal)/technews-html-master/main.js
@@ -7,3 +7,23 @@ ScrollReveal().reveal('.news-cards', { delay: 500 });
 ScrollReveal().reveal('.cards-banner-one', { delay: 500 });
 ScrollReveal().reveal('.cards-banner-two', { delay: 500 });
 
+// JavaScript para el video
+const btn = document.querySelector(".switch-btn");
+const video = document.querySelector(".video-container");
+
+btn.addEventListener("click", function () {
+  if (!btn.classList.contains("slide")) {
+    btn.classList.add("slide");
+    video.pause();
+  } else {
+    btn.classList.remove("slide");
+    video.play();
+  }
+});
+
+// preloader
+const preloader = document.querySelector(".preloader");
+
+window.addEventListener("load", function () {
+  preloader.classList.add("hide-preloader");
+});
